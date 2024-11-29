c85ed0e281a00b787ebc6889ddc87bc510685e43
Hannah Oliveira <hannah.clara.so@gmail.com>
2024-11-29 19:28:38 -0300

    Implementação da tela de Login

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Pikacu from "/public/img/detetivePikachu.png"
+
+
+export default function Login(){
+    
+
+    return (
+        <div className="flex gap-12 mx-24 mt-12 justify-center">
+        <Image src={Pikacu} alt="Pikachu usando uma lupa e com um chapéu marrom de detetive" width={512}/>
+
+        <form 
+        className="flex flex-col border-2 rounded-xl place-items-center w-4/12 justify-center"
+        >
+            <h1 className="mb-12 text-3xl font-bold mt-4">Login</h1>
+            <div className="flex flex-col">
+                <label htmlFor="email" className="ml-1 font-medium">E-mail</label>
+                <input type="email" name="email" id="email" 
+                placeholder="Digite seu e-mail" className="border-2 h-12 rounded-xl p-1.5 w-80 mb-12"/>
+            </div>
+
+            <div className="flex flex-col">
+                <label htmlFor="senha" className="ml-1 font-medium">Senha</label>
+                <input type="password" name="senha" id="senha" 
+                placeholder="Digite sua senha" className="border-2 h-12 rounded-xl p-1.5 w-80 mb-12"/>
+            </div>
+            
+            <button 
+            className="border-2 w-40 h-12 p-2 bg-orange-500 rounded-lg 
+            text-white text-lg font-bold hover:bg-orange-700 transition duration-200"
+            >Login</button>
+            
+            <p className="mt-4 font-medium">Não tem uma conta? <a href={'/registrar'} className="text-orange-500 font-medium text hover:underline">Registre-se!</a></p>
+        </form>
+        </div>
+    )
+}
+
