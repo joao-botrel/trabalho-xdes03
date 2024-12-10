305396a8bdd5c2673d36be72654cce718bb93406
André <meloluizandre10@gmail.com>
2024-12-10 14:57:03 -0300

    Conserto do layout da página de times

--- frontend/src/app/(main)/times/page.tsx
+++ frontend/src/app/(main)/times/page.tsx
@@ -2,7 +2,7 @@ import Time from '@/components/Time';
 
 export default function Times() {
 	return (
-		<div className="flex flex-col items-center h-fit w-5/6 max-w-6xl m-auto p-8 bg-orange-100/75 border-2 border-slate-300 rounded-xl">
+		<div className="flex flex-col items-center h-fit w-fit max-w-6xl m-auto p-8 bg-orange-100/75 border-2 border-slate-300 rounded-xl">
 			<div className="flex flex-col items-start gap-10">
 				<h1 className="text-3xl font-bold">Times</h1>
 				<div className="flex flex-col gap-8">
