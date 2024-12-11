339347cf3db37770e92a3e0bed61c25df744bc44
Victor Pasquini <victorcamposato@gmail.com>
2024-12-11 09:42:10 -0300

    Adicionando cors

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ import usuarioRouter from './routes/usuarios.routes.js'
 import pokemonRouter from './routes/pokemon.routes.js'
 import favoritoRouter from './routes/favoritos.routes.js'
 import timeRouter from './routes/times.routes.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 app.use(express.json());
+app.use(cors());
 
 app.use('/', usuarioRouter)
 app.use('/', pokemonRouter)
