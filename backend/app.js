efcf76349143295ff510210eca99b81f815ce862
Victor Pasquini <victorcamposato@gmail.com>
2024-12-11 10:01:49 -0300

    Ajustes merge

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import timeRouter from './routes/times.routes.js'
 import cors from 'cors'
 
 const app = express()
-const port = 3000
+const port = 3001
 app.use(express.json());
 app.use(cors());
 
