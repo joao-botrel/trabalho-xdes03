07bb67dcaa958ef8133db7f79a9ed98ad92adb21
André <meloluizandre10@gmail.com>
2024-12-13 00:02:07 -0300

    Adição da opção de favoritar pokemon

--- frontend/src/app/(main)/pokemons/[id]/page.tsx
+++ frontend/src/app/(main)/pokemons/[id]/page.tsx
@@ -6,104 +6,153 @@ import Image from 'next/image';
 import PokemonTipo from '@/components/PokemonTipo';
 import { useParams } from 'next/navigation';
 
-type PokemonType = 'fire' | 'water' | 'grass' | 'poison' | 'electric' | 'psychic' | 'normal' | 'ground' | 'rock' | 'bug' | 'fairy' | 'dragon' | 'dark' | 'steel' | 'ice' | 'fighting' | 'ghost' | 'flying';
+import StarEmpty from '/public/img/star.svg';
+import StarFilled from '/public/img/star-filled.svg';
+
+type PokemonType =
+	| 'fire'
+	| 'water'
+	| 'grass'
+	| 'poison'
+	| 'electric'
+	| 'psychic'
+	| 'normal'
+	| 'ground'
+	| 'rock'
+	| 'bug'
+	| 'fairy'
+	| 'dragon'
+	| 'dark'
+	| 'steel'
+	| 'ice'
+	| 'fighting'
+	| 'ghost'
+	| 'flying';
 
 interface Pokemon {
-    numero: number;
-    nome: string;
-    tipo1: PokemonType;
-    tipo2?: PokemonType; // Tipo 2 é opcional
-    gen: number;
-    descricao: string;
-    foto: string;
-    categoria: string;
+	numero: number;
+	nome: string;
+	tipo1: PokemonType;
+	tipo2?: PokemonType; // Tipo 2 é opcional
+	gen: number;
+	descricao: string;
+	foto: string;
+	categoria: string;
 }
 
 export default function Pokemon() {
-    const { id } = useParams();
-    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+	const { id } = useParams();
+	const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+	const [isFavorite, setIsFavorite] = useState(false);
 
-    useEffect(() => {
-        const fetchPokemon = async () => {
-            try {
-                const response = await axios.get<{ data: Pokemon }>(
-                    `http://localhost:3005/pokemon/numero/${id}`
-                );
-                setPokemon(response.data.data); // Acessa a propriedade `data` do response
-                setLoading(false);
-            } catch (err: any) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
+	useEffect(() => {
+		const fetchPokemon = async () => {
+			try {
+				const response = await axios.get<{ data: Pokemon }>(
+					`http://localhost:3005/pokemon/numero/${id}`
+				);
+				setPokemon(response.data.data); // Acessa a propriedade `data` do response
+				setLoading(false);
+			} catch (err: any) {
+				setError(err.message);
+				setLoading(false);
+			}
+		};
 
-        fetchPokemon();
-    }, [id]);
+		fetchPokemon();
+	}, [id]);
 
-    if (loading) {
-        return <p>Carregando...</p>;
-    }
+	if (loading) {
+		return <p>Carregando...</p>;
+	}
 
-    if (error) {
-        return <p>Erro: {error}</p>;
-    }
+	if (error) {
+		return <p>Erro: {error}</p>;
+	}
 
-    if (!pokemon) {
-        return <p>Nenhum Pokémon encontrado.</p>;
-    }
+	if (!pokemon) {
+		return <p>Nenhum Pokémon encontrado.</p>;
+	}
 
-    return (
-        <div className="h-screen">
-            <section className="flex items-center h-fit max-w-6xl m-auto p-8 bg-orange-100/75 border-2 border-slate-300 rounded-xl w-fit">
-                <div className="flex flex-col place-items-center">
-                    <div className="flex mt-2">
-                        <div
-                            id="imagemPokemon"
-                            className="w-[70%] place-items-center flex"
-                        >
-                            <Image
-                                src={pokemon.foto}
-                                alt={pokemon.nome}
-                                width={420}
-                                height={420}
-                            />
-                        </div>
-                        <div
-                            id="infoPokemon"
-                            className="flex flex-col ml-8 w-[60%]"
-                        >
-                            <p id="numPokemon" className="text-bold text-xl">
-                                N° {pokemon.numero}
-                            </p>
-                            <div className="flex">
-                                <p
-                                    id="genPokemon"
-                                    className="mt-1 mb-1 text-xl text-white text-center rounded-xl bg-orange-400 border border-orange-700 px-4 py-1"
-                                >
-                                    Gen {pokemon.gen}
-                                </p>
-                                <p
-                                    id="catPokemon"
-                                    className="mt-1 mb-1 ml-2 text-xl text-white text-center rounded-xl bg-orange-400 border border-orange-700 px-4 py-1"
-                                >
-                                    {pokemon.categoria}
-                                </p>
-                            </div>
-                            <h1 className="text-4xl font-semibold mb-1 mt-1">
-                                {pokemon.nome.charAt(0).toUpperCase() + pokemon.nome.slice(1)}
-                            </h1>
-                            <p className="text-xl mt-1 mb-1">Tipo</p>
-                            <PokemonTipo tipo={pokemon.tipo1} variant="lg" />
-                            {pokemon.tipo2 && <PokemonTipo tipo={pokemon.tipo2} variant="lg" />}
-                            <div className="bg-orange-200 mt-4 text-xl text-semibold h-[50%] pl-4 pt-4 rounded-xl">
-                                <p id="descricaoPokemon">{pokemon.descricao}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
+	return (
+		<div className="h-screen">
+			<section className="flex items-center h-fit max-w-6xl m-auto p-8 bg-orange-100/75 border-2 border-slate-300 rounded-xl w-fit">
+				<div className="flex flex-col place-items-center">
+					<div className="flex mt-2">
+						<div
+							id="imagemPokemon"
+							className="w-[70%] place-items-center flex"
+						>
+							<Image
+								src={pokemon.foto}
+								alt={pokemon.nome}
+								width={420}
+								height={420}
+							/>
+						</div>
+						<div
+							id="infoPokemon"
+							className="flex flex-col ml-8 w-[60%] relative"
+						>
+							<button className="absolute top-0 right-0">
+								{!isFavorite && (
+									<Image
+										src={StarEmpty}
+										alt="Favoritar Pokémon"
+										width={24}
+									/>
+								)}
+								{isFavorite && (
+									<Image
+										src={StarFilled}
+										alt="Desfavoritar Pokémon"
+										width={24}
+									/>
+								)}
+							</button>
+							<p id="numPokemon" className="text-bold text-xl">
+								N° {pokemon.numero}
+							</p>
+							<div className="flex">
+								<p
+									id="genPokemon"
+									className="mt-1 mb-1 text-xl text-white text-center rounded-xl bg-orange-400 border border-orange-700 px-4 py-1"
+								>
+									Gen {pokemon.gen}
+								</p>
+								<p
+									id="catPokemon"
+									className="mt-1 mb-1 ml-2 text-xl text-white text-center rounded-xl bg-orange-400 border border-orange-700 px-4 py-1"
+								>
+									{pokemon.categoria}
+								</p>
+							</div>
+							<h1 className="text-4xl font-semibold mb-1 mt-1">
+								{pokemon.nome.charAt(0).toUpperCase() +
+									pokemon.nome.slice(1)}
+							</h1>
+							<p className="text-xl mt-1 mb-1">Tipo</p>
+							<div className="flex flex-row gap-2">
+								<PokemonTipo
+									tipo={pokemon.tipo1}
+									variant="lg"
+								/>
+								{pokemon.tipo2 && (
+									<PokemonTipo
+										tipo={pokemon.tipo2}
+										variant="lg"
+									/>
+								)}
+							</div>
+							<div className="bg-orange-200 mt-4 text-xl text-semibold h-[50%] pl-4 pt-4 rounded-xl">
+								<p id="descricaoPokemon">{pokemon.descricao}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
